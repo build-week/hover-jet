e1e2f754eaa19e5c2518eba0d7c2afa45604a02d
Isaac Dykeman <ijdykeman@gmail.com>
2018-12-29 15:11:39 -0500

    fiducial memory bug fix

--- vision/fiducial_pose.cc
+++ vision/fiducial_pose.cc
@@ -1,23 +1,35 @@
-#include "vision/fiducial_pose.hh"
+#include "estimation/vision/fiducial_pose.hh"
+#include <cassert>
+#include <cstdlib>
 #include "eigen.hh"
 //%deps(opencv)
 
+namespace estimation {
 namespace vision {
 
-std::vector<MarkerDetection> detect_markers(const cv::Mat& inputImage) {
+std::pair<std::vector<cv::Vec<double, 3>>, std::vector<cv::Vec<double, 3>>> rvecs_tvecs(
+    std::vector<std::vector<cv::Point2f>> corners) {
+  std::vector<cv::Vec3d> rvecs, tvecs;
+  cv::Mat cameraMatrix = (cv::Mat1d(3, 3) << 320, 0, 320, 0, 320, 320, 0, 0, 1);
+  const cv::Mat distortionCoefficients = (cv::Mat1d(1, 8) << 0,0,0,0,0,0,0,0);
+  cv::aruco::estimatePoseSingleMarkers(corners, 0.49375, cameraMatrix,
+                                       distortionCoefficients, rvecs, tvecs);
+  return std::make_pair(rvecs, tvecs);
+}
+
+std::vector<MarkerDetection> detect_markers(cv::Mat inputImage) {
   cv::Ptr<cv::aruco::Dictionary> dictionary =
       cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
   std::vector<int> ids;
-  std::vector<std::vector<cv::Point2f> > corners;
+  std::vector<std::vector<cv::Point2f>> corners;
   cv::aruco::detectMarkers(inputImage, dictionary, corners, ids);
-  std::vector<cv::Vec3d> rvecs, tvecs;
+  auto [rvecs, tvecs] = rvecs_tvecs(corners);
 
-  cv::Mat cameraMatrix = (cv::Mat1d(3, 3) << 320, 0, 320, 0, 320, 320, 0, 0, 1);
-
-  const cv::Mat distortionCoefficients = (cv::Mat1d(1, 8) << 0, 0, 0, 0, 0);
-
-  cv::aruco::estimatePoseSingleMarkers(corners, 0.5, cameraMatrix,
-                                       distortionCoefficients, rvecs, tvecs);
+  for (auto const& quad : corners) {
+    for (auto const& center : quad) {
+      cv::circle(inputImage, center, 10, cv::Scalar(255, 0, 0));
+    }
+  }
   // The returned transformation is the one that transforms points from each marker
   // coordinate system to the camera coordinate system. The marker corrdinate system is
   // centered on the middle of the marker, with the Z axis perpendicular to the marker
@@ -25,13 +37,18 @@ std::vector<MarkerDetection> detect_markers(const cv::Mat& inputImage) {
   std::vector<MarkerDetection> detections;
   // draw axis for each marker
   for (int i = 0; i < ids.size(); i++) {
+    MarkerDetection detection;
+    detection.id = ids[i];
+
     const auto camera_from_marker_center =
         SE3(SO3::exp(jcc::Vec3(rvecs[i][0], rvecs[i][1], rvecs[i][2])),
             jcc::Vec3(tvecs[i][0], tvecs[i][1], tvecs[i][2]));
-
-    MarkerDetection detection;
     detection.marker_center_from_camera = camera_from_marker_center.inverse();
-    detection.id = ids[i];
+    if (abs(tvecs[i][0]) > 10e10) {
+      while (1) {
+      }
+    }
+    assert(abs(tvecs[i][0]) < 10e10);
     detections.push_back(detection);
   }
 
@@ -58,3 +75,4 @@ std::vector<MarkerInWorld> get_world_from_marker_centers(const cv::Mat& camera_i
 }
 
 }  // namespace vision
+}  // namespace estimation
