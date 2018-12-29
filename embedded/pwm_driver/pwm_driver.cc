adf90c193e7313f5d23f4245986d618b78fe2efc
David Ye <dvizye@gmail.com>
2018-12-29 03:16:28 +0000

    Switch PWM address

--- embedded/pwm_driver/pwm_driver.cc
+++ embedded/pwm_driver/pwm_driver.cc
@@ -16,7 +16,7 @@ https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library/
 #include <unistd.h> // usleep
 
 // This is for the PCA9685
-#define PCA9685_I2C_ADDR 0x40
+#define PCA9685_I2C_ADDR 0x42
 #define I2C_SLAVE 0x0703
 #define I2C_SLAVE_FORCE 0x0706
 
