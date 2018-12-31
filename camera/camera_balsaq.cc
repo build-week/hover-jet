fec4f8f472785f04d7f9f78ab51a34ad3a672927
Isaac <ijdykeman@gmail.com>
2018-12-30 22:02:17 -0500

    improve camera BQ speed

--- camera/camera_balsaq.cc
+++ camera/camera_balsaq.cc
@@ -1,5 +1,5 @@
-#include "vision/fiducial_detection_and_pose.hh"
 #include "camera/camera_balsaq.hh"
+#include "vision/fiducial_detection_and_pose.hh"
 
 #include "infrastructure/comms/mqtt_comms_factory.hh"
 // #include "infrastructure/comms/schemas/nop_structure_example.hh"
@@ -18,22 +18,27 @@ CameraBq::CameraBq() {
 
 void CameraBq::init() {
   std::cout << "INIT!" << std::endl;
-  cv::VideoCapture cap(0);
-
+  cap = cv::VideoCapture(0);
+  // 0 is the id of video device.0 if you have only one camera.
   publisher_ = make_publisher("pub_channel_name");
 }
 
 void CameraBq::loop() {
-  cv::VideoCapture stream1(0);
-      // 0 is the id of video device.0 if you have only one camera.
+  
   cv::Mat cameraFrame;
-  stream1.read(cameraFrame);
-  cv::imshow("cam", cameraFrame);
-  vision::detect_markers(cameraFrame);
-  publisher_->publish_raw("looping!");
+  if (cap.read(cameraFrame)) {
+    std::vector<vision::MarkerDetection> marker_detections =
+        vision::detect_markers(cameraFrame);
+    std::cout << "number of detections " << marker_detections.size()
+              << std::endl;
+    for (auto const& detection : marker_detections) {
+      std::cout << "detected artag #" << detection.id << std::endl;
+    }
+    publisher_->publish_raw("Camera balsa queue looping");
+  } else {
+    std::cout << "camera grab failed" << std::endl;
+  }
 }
-void CameraBq::shutdown() {
-  std::cout << "Shutting down!" << std::endl;
-}
+void CameraBq::shutdown() { std::cout << "Shutting down!" << std::endl; }
 
 }  // namespace jet
