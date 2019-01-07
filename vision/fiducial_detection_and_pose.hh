7f9817ebb1d99d41e11b89b2e729f630afe74846
Isaac <ijdykeman@gmail.com>
2019-01-06 23:12:37 -0500

    code cleanup

--- vision/fiducial_detection_and_pose.hh
+++ vision/fiducial_detection_and_pose.hh
@@ -36,8 +36,12 @@ std::vector<MarkerInWorld> get_world_from_marker_centers(const cv::Mat& camera_i
 void detect_board(const cv::Mat& input_image);
 
 const cv::Ptr<cv::aruco::Dictionary> aruco_dictionary =
-    cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    cv::aruco::getPredefinedDictionary(cv::aruco::DICT_5X5_250);
+
+constexpr float FIDUCIAL_WIDTH_METERS = 30.25 / 1000;
+constexpr float FIDUCIAL_GAP_WIDTH_METERS = 22.98 / 1000;
+
 const cv::Ptr<cv::aruco::GridBoard> aruco_board =
-    cv::aruco::GridBoard::create(4, 4, 35.15 / 1000, 35.15 / 1000, aruco_dictionary);
+    cv::aruco::GridBoard::create(4, 4, FIDUCIAL_WIDTH_METERS, FIDUCIAL_GAP_WIDTH_METERS, aruco_dictionary);
 
 }  // namespace jet
