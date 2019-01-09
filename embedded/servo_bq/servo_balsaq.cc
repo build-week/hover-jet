932d0a471969fc9e536ff350b1e5cacb86b21c50
Isaac <ijdykeman@gmail.com>
2019-01-09 00:16:41 -0500

    address comments

--- embedded/servo_bq/servo_balsaq.cc
+++ embedded/servo_bq/servo_balsaq.cc
@@ -2,12 +2,12 @@
 #include "embedded/servo_bq/servo_balsaq.hh"
 #include "infrastructure/balsa_queue/bq_main_macro.hh"
 
+#include <cassert>
 #include <chrono>
 #include <cstddef>
+#include <cstdlib>
 #include <iostream>
 #include <sstream>
-#include <cstdlib>
-
 
 //%deps(balsa_queue)
 //%deps(message)
@@ -17,21 +17,24 @@ namespace jet {
 ServoBq::ServoBq() {
   set_comms_factory(std::make_unique<MqttCommsFactory>());
 }
+
+namespace {
 constexpr int PWM_FREQUENCY = 330;
+}
 
 void ServoBq::init(int argc, char *argv[]) {
-  servo_index = atoi(argv[1]); // get the servo number in 0...3 from the command line
+  assert(argc == 1);
+  servo_index = atoi(argv[1]);  // get the servo number in 0...3 from the command line
 
   int i2cHandle = i2c_open("/dev/i2c-1");
   if (i2cHandle == -1) {
     std::cout << "Failed to open i2c" << std::endl;
-    // return -1;
   }
-  std::shared_ptr<PwmDriver> pwm_driver = std::make_shared<PwmDriver>(i2cHandle);
+  std::unique_ptr<PwmDriver> pwm_driver = std::make_unique<PwmDriver>(i2cHandle);
   pwm_driver->set_pwm_freq(PWM_FREQUENCY);
   pwm_driver->enable_auto_increment(true);
   servo = std::make_unique<ServoDriver>(
-      0, pwm_driver,
+      0, std::move(pwm_driver),
       "cfg/servo_cfg0.yaml");  // TODO create standard config dir
   subscriber_ = make_subscriber("servo_channel_" + std::to_string(servo_index));
 }
