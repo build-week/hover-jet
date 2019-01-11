8c4e67ad938be0d5d78f168f75b1c1909965485d
Isaac <ijdykeman@gmail.com>
2019-01-10 22:20:55 -0500

    manual change bq

--- embedded/servo_bq/servo_balsaq.cc
+++ embedded/servo_bq/servo_balsaq.cc
@@ -19,10 +19,11 @@ constexpr int PWM_FREQUENCY = 330;
 }
 
 void ServoBq::init(int argc, char *argv[]) {
-  assert(argc == 1);
-  servo_index = atoi(argv[1]);  // get the servo number in 0...3 from the command line
+  assert(argc == 3);
+  auto config_path = argv[1];
+  int servo_index = atoi(argv[2]);
 
-  int i2cHandle = i2c_open("/dev/i2c-1");
+  int i2cHandle = i2c_open(config_path);
   if (i2cHandle == -1) {
     std::cout << "Failed to open i2c" << std::endl;
   }
@@ -31,13 +32,16 @@ void ServoBq::init(int argc, char *argv[]) {
   pwm_driver->enable_auto_increment(true);
   servo = std::make_unique<ServoDriver>(
       0, std::move(pwm_driver),
-      "cfg/servo_cfg0.yaml");  // TODO create standard config dir
-  subscriber_ = make_subscriber("servo_channel_" + std::to_string(servo_index));
+      config_path);  // TODO create standard config dir
+  const std::string channel_name = "servo_channel_" + std::to_string(servo_index);
+  std::cout << "channel name " << channel_name << std::endl;
+  subscriber_ = make_subscriber(channel_name);
 }
 
 void ServoBq::loop() {
   SetServoMessage message;
   if (subscriber_->read(message, 1)) {
+    std::cout << "target angle is " << message.target_angle << std::endl;
     servo->set_angle(message.target_angle);
   }
 }
