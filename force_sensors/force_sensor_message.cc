1b6558daf7c7de7c028fbf4f440cbb1e5d930eb5
isaac <ijdykeman@gmail.com>
2019-01-12 02:54:35 +0000

    basic force sensor bq + message

---
+++ force_sensors/force_sensor_message.cc
@@ -0,0 +1,14 @@
+#include "camera/camera_image_message.hh"
+
+namespace jet {
+
+cv::Mat get_image_mat(CameraImageMessage message) {
+  cv::Mat result = cv::Mat(message.height, message.width, CV_8UC3);
+  const std::size_t n_elements = message.height * message.width * 3u;
+  constexpr std::size_t SIZE_OF_UCHAR = sizeof(uint8_t);
+  std::memcpy(result.data, message.image_data.data(),
+              SIZE_OF_UCHAR * n_elements);
+  return result;
+}
+
+}  // namespace jet
