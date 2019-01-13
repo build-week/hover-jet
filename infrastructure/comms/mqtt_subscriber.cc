e2d6ae8ee1b221d9fff69e1183d9603432103354
Jacob Panikulam <jpanikul@gmail.com>
2019-01-13 14:47:21 -0500

    Don't try to connect if the lock is held

--- infrastructure/comms/mqtt_subscriber.cc
+++ infrastructure/comms/mqtt_subscriber.cc
@@ -42,6 +42,9 @@ void MqttSubscriber::async_connect() {
     return;
   }
   if (connect_thread_) {
+    // @Ben: Leaving this here for a debug print in case the crash I saw happens again;
+    // I believe it's linux trapping a thread fault if this join gets called after detach
+    std::cout << "Attempting Join" << std::endl;
     connect_thread_->join();
   }
   connect_thread_ = std::make_unique<std::thread>([this]() { connect(); });
@@ -49,6 +52,14 @@ void MqttSubscriber::async_connect() {
 }
 
 void MqttSubscriber::connect() {
+  // Don't attempt to connect if we are currently connecting
+  // Note: We believe that the mqtt client should be thread-safe,
+  //       but it seems to deadlock if connect(...)->wait() called
+  //       from two threads
+  // TODO(jake, ben): Figure that out
+  if (connecting_) {
+    return;
+  }
   connecting_ = true;
   const int QOS = 1;
   try {
