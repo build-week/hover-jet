e65beba25462c4b01a098aa957fdc41791cfff65
Ben Kroop <benkroop@users.noreply.github.com>
2019-02-10 15:18:17 -0500

    Revert "Adding a Joystick based turbine control utility"

--- infrastructure/joystick/joystick_test.cc
+++ infrastructure/joystick/joystick_test.cc
@@ -2,8 +2,7 @@
 #include "infrastructure/joystick/joystick.hh"
 
 int main(int argc, char *argv[]) {
-  jet::Joystick joystick("/dev/input/js2");
-  // jet::Joystick joystick("/dev/input/by-id/usb-Thustmaster_Joystick_-_HOTAS_Warthog-joystick");
+  jet::Joystick joystick("/dev/input/js0");
   while (true) {
     auto event = joystick.read_event();
     if (!event) {
