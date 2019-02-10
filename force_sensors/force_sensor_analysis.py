06e6a7eb4438bc12a799c241c8f4327b560c0188
Jacob Panikulam <jpanikul@gmail.com>
2019-02-09 22:39:42 -0500

    More sensors stuff

--- force_sensors/force_sensor_analysis.py
+++ force_sensors/force_sensor_analysis.py
@@ -13,7 +13,9 @@ def parse(file):
     data = defaultdict(list)
     with open(file) as f:
         for ln in f:
-            data[int(ln[0])].append(float(ln[3:]))
+            name, ln_data = ln.split(': ')
+            ts, value = ln_data.split(',')
+            data[name].append((float(ts), float(value)))
 
     print list(map(len, data.values()))
 
@@ -23,8 +25,30 @@ def parse(file):
 def main():
     path = '/home/jacob/repos/hover-jet/bin/poopy'
     data = parse(path)
-    for name, entries in data.items():
-        plt.plot(range(len(entries)), entries, label='Sensor: {}'.format(name))
+    to_plot = [
+        'mag_utesla_x',
+        'mag_utesla_y',
+        'mag_utesla_z',
+        'turbine_rpm',
+        'pump_voltage',
+    ]
+
+    for element in to_plot:
+        assert element in data.keys()
+        el_data = np.vstack(data[element])
+        plt.plot(el_data[:, 0], el_data[:, 1], label=element)
+
+    # for name, entries in data.items():
+        # plt.plot(range(len(entries)), entries, label='Sensor: {}'.format(name))
+
+    # plt.plot(range(len(data[7])), data[7], label='Sensor: {}'.format("Mag-x"))
+    # plt.plot(range(len(data[8])), data[8], label='Sensor: {}'.format("Mag-y"))
+    # plt.plot(range(len(data[9])), data[9], label='Sensor: {}'.format("Mag-z"))
+
+    plt.title("Load Cell Output")
+    plt.ylabel("Force (un-normalized)")
+    plt.xlabel("Time (seconds)")
+    plt.legend()
     plt.show()
 
 
--- force_sensors/read_load_cells.cc
+++ force_sensors/read_load_cells.cc
@@ -1,4 +1,7 @@
+#include "embedded/imu_driver/imu_message.hh"
 #include "force_sensors/force_sensor_message.hh"
+#include "infrastructure/engine/turbine_state_message.hh"
+
 #include "infrastructure/logging/log_reader.hh"
 
 namespace jet {
@@ -11,17 +14,50 @@ void go() {
   const std::string turbine_state = "turbine_state";
   const std::string imu = "imu";
 
-  const std::vector<std::string> channels = {{force_sensor_output_channel,
-                                              servo_command_channel, turbine_ignition,
-                                              turbine_set_throttle, turbine_state, imu}};
+  const std::vector<std::string> channels = {
+      {force_sensor_output_channel, servo_command_channel, turbine_ignition, turbine_set_throttle, turbine_state, imu}};
 
-  const std::string log_path = "/jet/jet-test-1-15--2";
+  // const std::string log_path = "/jet/jet-test-1-15--2";
+  const std::string log_path = "/jet/test-logs/20190126231856";
   LogReader reader(log_path, channels);
 
-  for (int k = 0; k < 10000; ++k) {
+  uint64_t first_time = 0;
+  bool got_first_time = false;
+  for (;;) {
     ForceSensorMessage force_msg;
     if (reader.read_next_message(force_sensor_output_channel, force_msg)) {
-      std::cout << force_msg.id << ": " << force_msg.value << std::endl;
+      if (!got_first_time) {
+        first_time = force_msg.timestamp;
+        got_first_time = true;
+      }
+      const uint64_t t = uint64_t(force_msg.timestamp);
+      std::cout << "force " << force_msg.id << ": " << (t - first_time) << "," << force_msg.value << std::endl;
+    } else {
+      break;
+    }
+  }
+
+  for (;;) {
+    ImuMessage imu_msg;
+    if (reader.read_next_message(imu, imu_msg)) {
+      const uint64_t t = uint64_t(imu_msg.timestamp);
+
+      std::cout << "mag_utesla_x: " << (t - first_time) << "," << imu_msg.mag_utesla_x << std::endl;
+      std::cout << "mag_utesla_y: " << (t - first_time) << "," << imu_msg.mag_utesla_y << std::endl;
+      std::cout << "mag_utesla_z: " << (t - first_time) << "," << imu_msg.mag_utesla_z << std::endl;
+    } else {
+      break;
+    }
+  }
+
+  for (;;) {
+    TurbineStateMessage turb_msg;
+    if (reader.read_next_message(turbine_state, turb_msg)) {
+      const uint64_t t = turb_msg.header.timestamp_ns;
+      std::cout << "turbine_rpm: " << (t - first_time) << "," << turb_msg.turbine_rpm << std::endl;
+      std::cout << "pump_voltage: " << (t - first_time) << "," << turb_msg.pump_voltage << std::endl;
+    } else {
+      break;
     }
   }
 }
