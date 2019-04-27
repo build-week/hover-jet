c69fada1e675fbc292d263c2f1e6d7b7e7d647e9
Isaac Dykeman <ijdykeman@gmail.com>
2019-04-27 18:12:01 -0400

    create camera viewer bq (#114)
    Create camera viewer bq

---
+++ camera/camera_viewer_bq.cc
@@ -0,0 +1,35 @@
+//%bin(camera_viewer_main)
+#include "infrastructure/balsa_queue/bq_main_macro.hh"
+#include "camera/camera_viewer_bq.hh"
+
+namespace jet {
+
+void CameraViewerBq::init(const Config& config) {
+  subscriber_ = make_subscriber("camera_image_channel");
+}
+
+void CameraViewerBq::loop() {
+  CameraImageMessage image_message;
+
+  // Wait until we have the latest image_message
+  bool got_msg = false;
+  while (subscriber_->read(image_message, 1)) {
+    got_msg = true;
+  }
+
+  if (got_msg) {
+    gonogo_.go();
+    const cv::Mat camera_frame = get_image_mat(image_message);
+
+    cv::imshow("camera image", camera_frame);
+    cv::waitKey(1);
+  }
+}
+void CameraViewerBq::shutdown() {
+  std::cout << "Camera viewer shutting down." << std::endl;
+}
+
+}  // namespace jet
+
+BALSA_QUEUE_MAIN_FUNCTION(jet::CameraViewerBq)
+ 
