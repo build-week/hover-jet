12bde6871d93ec80bce96c9213ab01d09e199f60
Jacob Panikulam <jpanikul@gmail.com>
2019-04-27 17:20:17 -0400

    Update camera bq to use config

--- camera/camera_balsaq.hh
+++ camera/camera_balsaq.hh
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#include <infrastructure/balsa_queue/balsa_queue.hh>
+#include "infrastructure/balsa_queue/balsa_queue.hh"
 #include "camera/camera_image_message.hh"
 #include "infrastructure/comms/mqtt_comms_factory.hh"
 #include "camera/camera_manager.hh"
@@ -12,6 +12,24 @@
 
 namespace jet {
 
+// A C++ form of the yaml config
+//
+// Stored this way to avoid parsing overhead when re-reading the
+// config each frame
+//
+// The fields are left intentionally uninitialized, so that
+// their use before initialization can be detected with Valgrind
+struct CameraConfiguration {
+  int frames_per_second;
+  double exposure;
+
+  int auto_focus;
+  double auto_exposure;
+
+  int width_pixels;
+  int height_pixels;
+};
+
 class CameraBq : public BalsaQ {
  public:
   CameraBq() = default;
@@ -20,10 +38,12 @@ class CameraBq : public BalsaQ {
   void shutdown();
 
  private:
-  static const int CAMERA_FPS = 10;
+
   PublisherPtr publisher_;
   cv::VideoCapture cap;
+
   std::string camera_serial_number_;
+  CameraConfiguration camera_config_;
   CameraManager camera_manager_ = CameraManager();
 };
 
