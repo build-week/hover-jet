2ef6e8a9cc248dcadf96050dd9e43ecf61c6913f
Jacob Panikulam <jpanikul@gmail.com>
2019-05-13 20:38:30 -0400

    Add a nice patch for mason

--- embedded/imu_driver/imu_demo.cc
+++ embedded/imu_driver/imu_demo.cc
@@ -9,48 +9,46 @@
 namespace jet {
 namespace embedded {
 
-void go() {
-  ImuDriver driver_1;
-  ImuDriver driver_2;
-  driver_1.initialize("/dev/i2c-1", 0x28);
-  driver_2.initialize("/dev/i2c-1", 0x29);
+void go(const uint8_t imu_id) {
+  ImuDriver driver;
+  const bool initialized_successfully = driver.initialize("/dev/i2c-1", imu_id);
+  assert(initialized_successfully);
 
   while (true) {
     const auto t1 = jcc::now();
-    const Eigen::Vector3d accel_mpss_dr1 = driver_1.read_accel_mpss().transpose();
-    if (accel_mpss_dr1 == Eigen::Vector3d::Zero()) {
-      std::cout << "(1) All zeros, attempting to reinitialize" << std::endl;
-      driver_1.initialize("/dev/i2c-1", 0x28);
+    const Eigen::Vector3d drv = driver.read_accel_mpss().transpose();
+    if (drv == Eigen::Vector3d::Zero()) {
+      std::cout << "(1) All zeros, this is the problem, @Mason" << std::endl;
+      // Reinitializing seems to fix it, uncomment the next line if you want to try that
+      // driver.initialize("/dev/i2c-1", imu_id);
     }
 
-    std::cout << "\tAccel(1): " << driver_1.read_accel_mpss().transpose() << "\n";
-    std::cout << "\tGyro(1) : " << driver_1.read_gyro_radps().transpose() << "\n";
-    std::cout << "\tMag(1)  : " << driver_1.read_magnetometer_utesla().transpose() << std::endl;
+    std::cout << "\tAccel(1): " << driver.read_accel_mpss().transpose() << "\n";
+    std::cout << "\tGyro(1) : " << driver.read_gyro_radps().transpose() << "\n";
+    std::cout << "\tMag(1)  : " << driver.read_magnetometer_utesla().transpose() << std::endl;
     const auto t2 = jcc::now();
-    std::cout << "-------" << std::endl;
+    std::cout << "(Sampling time: " << estimation::to_seconds(t2 - t1) << ") -------" << std::endl;
 
-    const Eigen::Vector3d accel_mpss_dr2 = driver_2.read_accel_mpss().transpose();
-    if (accel_mpss_dr2 == Eigen::Vector3d::Zero()) {
-      std::cout << "(2) All zeros, attempting to reinitialize" << std::endl;
-      driver_2.initialize("/dev/i2c-1", 0x29);
-    }
-    std::cout << "\tAccel(2): " << accel_mpss_dr2 << "\n";
-    std::cout << "\tGyro(2) : " << driver_2.read_gyro_radps().transpose() << "\n";
-    std::cout << "\tMag(2)  : " << driver_2.read_magnetometer_utesla().transpose() << std::endl;
-    const auto t3 = jcc::now();
-
-    std::cout << "\n\n" << std::endl;
-    std::cout << "IMU 1: " << estimation::to_seconds(t2 - t1) << "s" << std::endl;
-    std::cout << "IMU 2: " << estimation::to_seconds(t3 - t2) << "s" << std::endl;
-    std::cout << "Total: " << estimation::to_seconds(t3 - t1) << "s" << std::endl;
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(driver_1.sample_period_ms()));
+    std::this_thread::sleep_for(std::chrono::milliseconds(driver.sample_period_ms()));
   }
 }
 
 }  // namespace embedded
 }  // namespace jet
 
-int main() {
-  jet::embedded::go();
+// Pass *any* argument, and this will use imu 2
+//
+int main(int argc, const char** argv) {
+  const bool use_imu_2 = argc > 1;
+
+  constexpr uint8_t IMU_1_ID = 0x28;
+  constexpr uint8_t IMU_2_ID = 0x29;
+
+  const uint8_t imu_id = use_imu_2 ? IMU_2_ID : IMU_1_ID;
+
+  const std::string report_string = use_imu_2 ? "Opening IMU 2" : "Opening IMU 1";
+
+  std::cout << report_string << std::endl;
+
+  jet::embedded::go(imu_id);
 }
