38b94515558c0a892c4c19b6492c556ef4de36d9
Jacob Panikulam <jpanikul@gmail.com>
2019-06-30 18:04:38 -0400

    IMU demo

--- embedded/imu_driver/imu_demo.cc
+++ embedded/imu_driver/imu_demo.cc
@@ -17,20 +17,26 @@ void go(const std::string& bus, const uint8_t imu_id) {
   std::cout << "IMU ID: " << driver.imu_guid() << std::endl;
   std::cout << "ID: " << static_cast<int>(driver.imu_guid().bytes()[0]) << std::endl;
 
+  auto t0 = jcc::now();
+
   while (true) {
     const auto t1 = jcc::now();
     const Eigen::Vector3d drv = driver.read_accel_mpss().transpose();
     if (drv == Eigen::Vector3d::Zero()) {
       std::cout << "(1) All zeros, this is the problem, @Mason" << std::endl;
       // Reinitializing seems to fix it, uncomment the next line if you want to try that
-      // driver.initialize("/dev/i2c-1", imu_id);
+      driver.initialize("/dev/i2c-1", imu_id);
     }
 
-    //std::cout << "\tAccel(1): " << driver.read_accel_mpss().transpose() << "\n";
-    //std::cout << "\tGyro(1) : " << driver.read_gyro_radps().transpose() << "\n";
-    //std::cout << "\tMag(1)  : " << driver.read_magnetometer_utesla().transpose() << std::endl;
-    //const auto t2 = jcc::now();
-    //std::cout << "(Sampling time: " << estimation::to_seconds(t2 - t1) << ") -------" << std::endl;
+    driver.read_accel_mpss().transpose();
+    driver.read_gyro_radps().transpose();
+    driver.read_magnetometer_utesla().transpose();
+    const auto t2 = jcc::now();
+
+    if (estimation::to_seconds(t2 - t0) > 4.0) {
+      std::cout << "(Sampling time: " << estimation::to_seconds(t2 - t1) << ") -------" << std::endl;
+      t0 = t2;
+    }
 
     std::this_thread::sleep_for(std::chrono::milliseconds(driver.sample_period_ms()));
   }
