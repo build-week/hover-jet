a7b38c9af69f10db6e5ab62c10b5d97e1f86e7d3
Anthony Olive <sentree@iris-systems.net>
2019-07-02 21:43:53 -0400

    Add an environment bootstrapping step to readme.md
    In order to line up with the `Environment Setup` wiki page, we need to
    ensure that users have run the bootstrapping script and are able to use
    the subcommands under `jet`. To do this, the `How to pull` section
    needed to be moved before the `How to get the Docker image` section. In
    a later commit, the rest of the commands in `readme.md` will be changed
    to `jet` subcommands.

--- infrastructure/scripts/environment_bootstrap.sh
+++ infrastructure/scripts/environment_bootstrap.sh
@@ -2,4 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Add the hover-jet repository's "scripts" directory to path for new shells
 echo "export PATH=\$PATH:"$DIR >> ~/.bashrc
+
+# Source the modified "~/.bashrc" file in this shell
+source ~/.bashrc
