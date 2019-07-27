88df4d5b6ebe96eb9547c8eae43c70bd460e23a4
mason turner <legostrat@gmail.com>
2019-07-27 18:05:32 +0000

    Change formatting a little and add todo

--- infrastructure/logging/log_summarizer.hh
+++ infrastructure/logging/log_summarizer.hh
@@ -37,6 +37,7 @@ struct LogSummary {
         channel_stats[channel_name].first_message_received = message.header.timestamp_ns;
         channel_stats[channel_name].num_messages_received++;
       }
+      // TODO(mason): Refactor to not assign last message received every time
       while(log_reader.read_next_message(channels[i], message)) {
         channel_stats[channel_name].last_message_received = message.header.timestamp_ns;
         channel_stats[channel_name].num_messages_received++;
@@ -48,10 +49,19 @@ struct LogSummary {
   }
 
   void print() {
+    // Output Header in Bold Red
     const std::string reset("\033[0m");
     const std::string red_bold("\033[31;1m");
-    std::cout << red_bold << std::setw(30) << "Channel" << std::setw(25) << "First Timestamp" << std::setw(25) << "Last Timestamp" << std::setw(17) << "Duration (s)" << std::setw(15) << "Num Messages" << std::setw(15) << "Frequency (Hz)" << std::endl;
+    std::cout << red_bold << std::setw(30) << "Channel" 
+                          << std::setw(25) << "First Timestamp" 
+                          << std::setw(25) << "Last Timestamp" 
+                          << std::setw(17) << "Duration (s)" 
+                          << std::setw(15) << "Num Messages" 
+                          << std::setw(17) << "Frequency (Hz)" 
+                          << std::endl;
     std::cout << reset;
+
+    // Output all statistics
     for(auto it = channel_stats.begin(); it != channel_stats.end(); it++) {
       const auto duration = (it->second.last_message_received - it->second.first_message_received)/static_cast<double>(1e9); 
       std::cout << std::setw(30) << it->first << 
@@ -59,7 +69,7 @@ struct LogSummary {
                    std::setw(25) << std::fixed << std::setprecision(9) << it->second.last_message_received/static_cast<double>(1e9) << 
                    std::setw(17) << std::fixed << std::setprecision(3) << duration << 
                    std::setw(15) << it->second.num_messages_received << 
-                   std::setw(15) << std::fixed << std::setprecision(3) << it->second.frequency << std::endl;
+                   std::setw(17) << std::fixed << std::setprecision(3) << it->second.frequency << std::endl;
     }
   }
 };
