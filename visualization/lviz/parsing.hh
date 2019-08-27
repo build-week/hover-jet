90e8757a3adb599c7830df480e4702e4c4902081
Isaac <ijdykeman@gmail.com>
2019-08-27 12:21:18 -0400

    add colormaps

--- visualization/lviz/parsing.hh
+++ visualization/lviz/parsing.hh
@@ -23,25 +23,28 @@ using SceneUpdate = std::variant<ColoredPoint, std::pair<ColoredPoint, ColoredPo
 
 std::string FLOAT_STR = ("[+-]?(?:\\d+(?:[.]\\d*)?(?:e[+-]?\\d+)?|[.]\\d+(?:e[+-]?\\d+)?)");
 std::string WHITESPACE_STR = "\\s+";
+
+// TODO instead have a master list of all color names and generate these regexes
 std::string ALL_COLOR_NAMES_STR = "(?:(?:red)|(?:green)|(?:blue)|(?:orange)|(?:magenta)|(?:violet)|(?:cyan))";
 std::string VEC3_STR =
     "(" + FLOAT_STR + ")" + WHITESPACE_STR + "(" + FLOAT_STR + ")" + WHITESPACE_STR + "(" + FLOAT_STR + ")";
 std::string RGB_COLOR_STR = "(?:" + FLOAT_STR + WHITESPACE_STR + FLOAT_STR + WHITESPACE_STR + FLOAT_STR + ")";
-std::string VIRIDIS_COLOR_STR = "(?:viridis" + WHITESPACE_STR + FLOAT_STR + ")";
-std::string COLOR = "(" + RGB_COLOR_STR + "|" + ALL_COLOR_NAMES_STR + "|" + VIRIDIS_COLOR_STR + ")";
+std::string COLORMAP_COLOR_STR = "(?:(?:(?:viridis)|(?:inferno))" + WHITESPACE_STR + FLOAT_STR + ")";
+std::string COLOR = "(" + RGB_COLOR_STR + "|" + ALL_COLOR_NAMES_STR + "|" + COLORMAP_COLOR_STR + ")";
 
-std::regex RGB_COLOR_REGEX("(" + FLOAT_STR + ")" + WHITESPACE_STR + "(" + FLOAT_STR + ")" + WHITESPACE_STR + "(" +
-                           FLOAT_STR + ")");
+std::regex RGB_COLOR_REGEX("^(" + FLOAT_STR + ")" + WHITESPACE_STR + "(" + FLOAT_STR + ")" + WHITESPACE_STR + "(" +
+                           FLOAT_STR + ")$");
 
-std::regex VIRIDIS_COLOR_REGEX("(?:viridis" + WHITESPACE_STR + "(" + FLOAT_STR + "))");
+// TODO instead have a master list of all colormap names and generate these regexes
+std::regex COLORMAP_COLOR_REGEX("^(?:((?:viridis)|(?:inferno))" + WHITESPACE_STR + "(" + FLOAT_STR + "))$");
 
-std::regex ALL_COLOR_NAMES_REGEX(ALL_COLOR_NAMES_STR);
+std::regex ALL_COLOR_NAMES_REGEX("^" + ALL_COLOR_NAMES_STR + "$");
 
 std::regex POINT_REGEX("^point" + WHITESPACE_STR + VEC3_STR + WHITESPACE_STR + COLOR + WHITESPACE_STR + "(" + FLOAT_STR +
                        ")$");
 
 std::regex LINE_REGEX("^line" + WHITESPACE_STR + VEC3_STR + WHITESPACE_STR + COLOR + WHITESPACE_STR + VEC3_STR +
-                      WHITESPACE_STR + COLOR);
+                      WHITESPACE_STR + COLOR + "$");
 
 std::regex NEW_FRAME_REGEX("^new_frame$");
 std::regex SET_FRAME_TIME_REGEX("^set_frame_time" + WHITESPACE_STR + "(" + FLOAT_STR + ")$");
@@ -56,6 +59,7 @@ Eigen::Vector3f vec3_from_strings(const std::tuple<std::string, std::string, std
 }
 
 rgb_color parse_color(const std::string &color_string) {
+
   std::smatch matches;
   if (std::regex_search(color_string, matches, RGB_COLOR_REGEX)) {
     return rgb_color{stof(matches[1]) / 255.0, stof(matches[2]) / 255.0, stof(matches[3]) / 255.0};
@@ -77,17 +81,28 @@ rgb_color parse_color(const std::string &color_string) {
       return rgb_color{42.0 / 255.0, 161.0 / 255.0, 152.0 / 255.0};
     }
   }
-  else if (std::regex_search(color_string, matches, VIRIDIS_COLOR_REGEX)) {
-    auto value = stof(matches[1]);
+  else if (std::regex_search(color_string, matches, COLORMAP_COLOR_REGEX)) {
+
+    auto value = stof(matches[2]);
     value = std::clamp(value, 0.0f, 1.0f);
-    const auto color = tinycolormap::GetColor(value, tinycolormap::ColormapType::Viridis);
+
+
+    tinycolormap::ColormapType colormap;
+    const auto colormap_name = matches[1];
+    if (colormap_name == "viridis"){
+        colormap = tinycolormap::ColormapType::Viridis;
+    }
+    else if (colormap_name == "inferno"){
+        colormap = tinycolormap::ColormapType::Inferno;
+    }
+    else{
+        std::cerr << "ERROR: unrecognized colormap " << colormap_name << " . Substituting viridis." << std::endl;
+        colormap = tinycolormap::ColormapType::Viridis;
+    }
+    const auto color = tinycolormap::GetColor(value, colormap);
     return rgb_color{(float)color.r(), (float)color.g(), (float)color.b()};
-
-
   }
-
-
-  std::cout << "ERROR: could not parse color " << matches[0] << " . Substituting white.";
+  std::cerr << "ERROR: could not parse color " << matches[0] << " . Substituting white." << std::endl;
   return rgb_color{1,1,1};
 }
 
@@ -114,16 +129,16 @@ std::optional<SceneUpdate> scene_element_from_line(const std::string &line) {
         }
     }
     
-    Eigen::Vector3f location1;
-    location1 << stof(matches[1]), stof(matches[2]), stof(matches[3]);
-    auto color1 = parse_color(matches[4]);
+    Eigen::Vector3f line_endpoint_1;
+    line_endpoint_1 << stof(matches[1]), stof(matches[2]), stof(matches[3]);
+    auto color_1 = parse_color(matches[4]);
 
-    Eigen::Vector3f location2;
-    location2 << stof(matches[5]), stof(matches[6]), stof(matches[7]);
-    auto color2 = parse_color(matches[8]);
+    Eigen::Vector3f line_endpoint_2;
+    line_endpoint_2 << stof(matches[5]), stof(matches[6]), stof(matches[7]);
+    auto color_2 = parse_color(matches[8]);
     return std::make_optional(
-        std::make_pair(ColoredPoint{location1.x(), location1.z(), location1.y(), color1.r, color1.g, color1.b, 1},
-                       ColoredPoint{location2.x(), location2.z(), location2.y(), color2.r, color2.g, color2.b, 1}));
+        std::make_pair(ColoredPoint{line_endpoint_1.x(), line_endpoint_1.z(), line_endpoint_1.y(), color_1.r, color_1.g, color_1.b, 1},
+                       ColoredPoint{line_endpoint_2.x(), line_endpoint_2.z(), line_endpoint_2.y(), color_2.r, color_2.g, color_2.b, 1}));
   } else if (std::regex_search(line, matches, NEW_FRAME_REGEX)) {
     return std::make_optional(NewScene{});
   } else if (std::regex_search(line, matches, SET_FRAME_TIME_REGEX)) {
